7448ad30b4f274c4aea2c958e61ee48c5e1d65c1
Osvaldo Margato <15236942+margato@users.noreply.github.com>
2020-02-27 23:24:49 -0300

    Add redirect when page is not found

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndexPage from "./pages";
+import NotFound from "./pages/404";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
-ReactDOM.render(<IndexPage />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+      <Switch>
+        <Route exact path="/">
+          <IndexPage />
+        </Route>
+        <Route path="/**">
+          <NotFound />
+        </Route>
+      </Switch>
+  </Router>
+, document.getElementById('root'));
 
